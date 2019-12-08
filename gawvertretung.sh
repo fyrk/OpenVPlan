1c6e4b33c2e61d887739bf8339acecc6ce03a731
Florian Rädiker <38762910+FlorianRaediker@users.noreply.github.com>
2019-12-08 13:26:17 +0100

    gawvertretung.sh: no output to nohup.out

--- gawvertretung.sh
+++ gawvertretung.sh
@@ -2,7 +2,7 @@
 cd ~/gawvertretung
 export PYTHONPATH=${PYTHONPATH}:${HOME}/asynctelebot
 echo "$PYTHONPATH"
-nohup python3.7 build_snippets.py 2> output/build_snippets.txt &
-nohup python3.7 bot_sender.py 2> output/bot_sender.txt &
-nohup python3.7 bot_listener_students.py 2> output/bot_listener_students.txt &
-nohup python3.7 bot_listener_teachers.py 2> output/bot_listener_teachers.txt &
+nohup python3.7 build_snippets.py > /dev/null 2> output/build_snippets.txt &
+nohup python3.7 bot_sender.py > /dev/null > /dev/null 2> output/bot_sender.txt &
+nohup python3.7 bot_listener_students.py > /dev/null 2> output/bot_listener_students.txt &
+nohup python3.7 bot_listener_teachers.py > /dev/null 2> output/bot_listener_teachers.txt &
