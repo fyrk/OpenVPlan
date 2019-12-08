6cd24e3a701efafcb00cf3459ff262538f82be3e
Florian Rädiker <38762910+FlorianRaediker@users.noreply.github.com>
2019-12-08 13:18:46 +0100

    fixed gawvertretung.sh

--- gawvertretung.sh
+++ gawvertretung.sh
@@ -2,7 +2,7 @@
 cd ~/gawvertretung
 export PYTHONPATH=${PYTHONPATH}:${HOME}/asynctelebot
 echo "$PYTHONPATH"
-nohup python3.7 build_snippets.py 2> output/build_snippets.txt
-nohup python3.7 bot_sender.py 2> output/bot_sender.txt
-nohup python3.7 bot_listener_students.py 2> output/bot_listener_students.py
-nohup python3.7 bot_listener_teachers.py 2> output/bot_listener_teachers.py
+nohup python3.7 build_snippets.py 2> output/build_snippets.txt &
+nohup python3.7 bot_sender.py 2> output/bot_sender.txt &
+nohup python3.7 bot_listener_students.py 2> output/bot_listener_students.py &
+nohup python3.7 bot_listener_teachers.py 2> output/bot_listener_teachers.py &
