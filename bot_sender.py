a8bc2af336af2a55ce6c3b16b93524f2bfacbfad
Florian Rädiker <38762910+FlorianRaediker@users.noreply.github.com>
2019-12-16 14:26:24 +0100

    fixed bot not sending messages

--- bot_sender.py
+++ bot_sender.py
@@ -19,29 +19,19 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 
 class BotSender(FileSystemEventHandler):
-    def __init__(self, bot_token_students, bot_token_teachers, create_connection_callback, loop):
+    def __init__(self, bot_token_students, bot_token_teachers, connection, loop):
         self.__bot_token_students = bot_token_students
         self.__bot_token_teachers = bot_token_teachers
         self.connection_students = None
         self.connection_teachers = None
-        self.bot_students = None
-        self.bot_teachers = None
-        self.sender_students = None
-        self.sender_teachers = None
-        self.create_connection = create_connection_callback
-        self.loop = loop
-
-        self.last_status = None
-
-    def _create_bots_if_necessary(self):
-        # if self.connection is None:
-        if self.connection:
-            self.connection.close()
-        self.connection, db_commands = self.create_connection()
         self.bot_students = StudentDatabaseBot(self.__bot_token_students, self.connection)
         self.bot_teachers = TeacherDatabaseBot(self.__bot_token_teachers, self.connection)
         self.sender_students = StudentMessageSender(self.bot_students, "data/sent_messages_students.json")
         self.sender_teachers = TeacherMessageSender(self.bot_teachers, "data/sent_messages_teachers.json")
+        self.connection = connection
+        self.loop = loop
+
+        self.last_status = None
 
     def on_modified(self, event):
         if event.src_path == "data/substitutions/substitutions.pickle":
@@ -59,7 +49,6 @@ class BotSender(FileSystemEventHandler):
         else:
             if status != self.last_status:
                 logger.info(f"Status changed, sending messages {data_students}")
-                self._create_bots_if_necessary()
                 self.last_status = status
                 logger.info("set event loop")
                 asyncio.set_event_loop(self.loop)
@@ -81,6 +70,8 @@ if __name__ == "__main__":
 
     with open("bot/secret.json", "r") as f:
         secret = json.load(f)
+
+    connection = get_connection(secret)
 
     sender = BotSender(secret["token_students"], secret["token_teachers"], partial(get_connection, secret=secret),
                        asyncio.new_event_loop())
