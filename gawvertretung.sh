ab8cdda5e58eabfae7c0d449dc365178d0189361
Florian Rädiker <38762910+FlorianRaediker@users.noreply.github.com>
2019-12-22 18:24:04 +0100

    webhook doesn't need to be added to gawvertretung.sh ...

--- gawvertretung.sh
+++ gawvertretung.sh
@@ -4,4 +4,3 @@ export PYTHONPATH=${PYTHONPATH}:${HOME}/asynctelebot
 echo "$PYTHONPATH"
 nohup python3.7 build_snippets.py > /dev/null 2> output/build_snippets.txt &
 nohup python3.7 bot_sender.py > /dev/null > /dev/null 2> output/bot_sender.txt &
-nohup python3.7 bot_listener_webhook.py > /dev/null 2> output/bot_listener_webhook.txt &
