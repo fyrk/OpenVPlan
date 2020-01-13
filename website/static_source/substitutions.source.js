e3fcd6f4498beb4e21216c33cdac8ae78eb48196
Florian Rädiker <38762910+FlorianRaediker@users.noreply.github.com>
2020-01-13 20:45:33 +0100

    added service worker

--- website/static_source/substitutions.source.js
+++ website/static_source/substitutions.source.js
@@ -101,3 +101,13 @@ function update() {
 }
 
 window.onfocus = update;
+
+/* if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register('/js/service-worker.js')
+      .then(function(reg){
+        console.log("Yes, it did.");
+     }).catch(function(err) {
+        console.log("No it didn't. This happened:", err)
+    });
+ }*/
