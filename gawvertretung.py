fd6e822e59003597ddac969bb245940fb5e0cb51
Florian Rädiker <38762910+FlorianRaediker@users.noreply.github.com>
2020-02-14 20:22:54 +0100

    Send robots tags in headers (noarchive, notranslate, for selections: noindex) and remove "robots" meta tag from HTML

--- gawvertretung.py
+++ gawvertretung.py
@@ -71,8 +71,8 @@ class SubstitutionPlan:
         self.current_status_string = ""
         self.data_students = []
         self.data_teachers = []
-        self.index_site_students = ""
-        self.index_site_teachers = ""
+        self.index_site_students = b""
+        self.index_site_teachers = b""
 
     async def async_init(self):
         await self._try_load_substitutions_from_file()
@@ -151,42 +151,75 @@ class SubstitutionPlan:
 
     async def _create_sites(self):
         t1 = time.perf_counter_ns()
-        self.index_site_students, self.index_site_teachers = await asyncio.gather(
+        index_students, index_teachers = await asyncio.gather(
             self.templates.render_substitution_plan_students(self.current_status_string, self.data_students),
             self.templates.render_substitution_plan_teachers(self.current_status_string, self.data_teachers)
         )
+        self.index_site_students = index_students.encode("utf-8")
+        self.index_site_teachers = index_teachers.encode("utf-8")
         t2 = time.perf_counter_ns()
         logger.debug(f"Index sites created in {t2 - t1}ns")
 
-    async def get_site_students(self, storage):
+    async def application_students(self, environ, start_response):
         # noinspection PyBroadException
         try:
             await self.update_data()
+            storage = urllib.parse.parse_qs(environ["QUERY_STRING"])
             if "s" in storage:
                 selection = split_selection(",".join(storage["s"]))
                 if selection:
-                    return "200 OK", await self.templates.render_substitution_plan_students(
+                    content = (await self.templates.render_substitution_plan_students(
                         self.current_status_string, self.data_students,
-                        [s.upper() for s in selection], ", ".join(selection))
-            return "200 OK", self.index_site_students
+                        [s.upper() for s in selection], ", ".join(selection))).encode("utf-8")
+                    start_response("200 OK", [("Content-Type", "text/html;charset=utf-8"),
+                                              ("Content-Length", str(len(content))),
+                                              ("X-Robots-Tag", "noindex")])
+                    return [content]
+            start_response("200 OK", [("Content-Type", "text/html;charset=utf-8"),
+                                      ("Content-Length", str(len(self.index_site_students))),
+                                      ("X-Robots-Tag", "nocache, notranslate")])
+            return [self.index_site_students]
         except Exception:
             logger.exception("Exception occurred")
-            return "500 Internal Server Error", await self.templates.render_error_500_students()
+            content = (await self.templates.render_error_500_students()).encode("utf-8")
+            start_response("500 Internal Server Error", [("Content-Type", "text/html;charset=utf-8"),
+                                                         ("Content-Length", str(len(content))),
+                                                         ("X-Robots-Tag", "nocache, notranslate")])
+            return [content]
 
-    async def get_site_teachers(self, storage):
+    async def application_teachers(self, environ, start_response):
         # noinspection PyBroadException
         try:
             await self.update_data()
+            storage = urllib.parse.parse_qs(environ["QUERY_STRING"])
             if "s" in storage:
                 selection = sorted(split_selection(",".join(storage["s"])))
                 if selection:
-                    return "200 OK", await self.templates.render_substitution_plan_teachers(
+                    content = (await self.templates.render_substitution_plan_teachers(
                         self.current_status_string, self.data_teachers, [
-                            s.upper() for s in selection], ", ".join(selection).upper())
-            return "200 OK", self.index_site_teachers
+                            s.upper() for s in selection], ", ".join(selection).upper())).encode("utf-8")
+                    start_response("200 OK", [("Content-Type", "text/html;charset=utf-8"),
+                                              ("Content-Length", str(len(content))),
+                                              ("X-Robots-Tag", "noindex")])
+                    return [content]
+            start_response("200 OK", [("Content-Type", "text/html;charset=utf-8"),
+                                      ("Content-Length", str(len(self.index_site_teachers))),
+                                      ("X-Robots-Tag", "nocache, notranslate")])
+            return [self.index_site_teachers]
         except Exception:
             logger.exception("Exception occurred")
-            return "500 Internal Server Error", await self.templates.render_error_500_teachers()
+            content = (await self.templates.render_error_500_teachers()).encode("utf-8")
+            start_response("500 Internal Server Error", [("Content-Type", "text/html;charset=utf-8"),
+                                                         ("Content-Length", str(len(content))),
+                                                         ("X-Robots-Tag", "nocache, notranslate")])
+            return [content]
+
+    async def application_post(self, environ, start_response):
+        response, content = asyncio.run(self.get_current_status())
+        content = content.encode("utf-8")
+        start_response(response, [("Content-Type", "text/text;charset=utf-8"),
+                                  ("Content-Length", str(len(content)))])
+        return [content]
 
     async def get_current_status(self):
         # noinspection PyBroadException
@@ -203,21 +236,19 @@ asyncio.run(substitution_plan.async_init())
 
 
 async def async_application(environ, start_response):
+    t1 = time.perf_counter_ns()
     try:
         substitution_plan.stats.new_request(environ)
         if environ["PATH_INFO"].startswith("/api"):
             return await substitution_plan.api.application(environ["PATH_INFO"][4:], environ, start_response)
 
-        t1 = time.perf_counter_ns()
         if environ["REQUEST_METHOD"] == "GET":
             if environ["PATH_INFO"] == "/":
                 logger.info("GET /")
-                storage = urllib.parse.parse_qs(environ["QUERY_STRING"])
-                response, content = await substitution_plan.get_site_students(storage)
+                return await substitution_plan.application_students(environ, start_response)
             elif environ["PATH_INFO"] == "/teachers":
                 logger.info("GET /teachers")
-                storage = urllib.parse.parse_qs(environ["QUERY_STRING"])
-                response, content = await substitution_plan.get_site_teachers(storage)
+                return await substitution_plan.application_teachers(environ, start_response)
             elif environ["PATH_INFO"] == "/privacy":
                 logger.info("GET /privacy")
                 response = "200 OK"
@@ -231,31 +262,23 @@ async def async_application(environ, start_response):
                 response = "404 Not Found"
                 content = await substitution_plan.templates.render_error_404()
             content = content.encode("utf-8")
-            t2 = time.perf_counter_ns()
-            logger.debug(f"Time for handling request: {t2 - t1}ns")
             start_response(response, [("Content-Type", "text/html;charset=utf-8"),
                                       ("Content-Length", str(len(content)))])
             return [content]
 
         if environ["REQUEST_METHOD"] == "POST" and environ["PATH_INFO"] == "/":
             logger.info("POST /")
-            response, content = asyncio.run(substitution_plan.get_current_status())
-            content = content.encode("utf-8")
-            t2 = time.perf_counter_ns()
-            logger.debug(f"Time for handling request: {t2 - t1}ns")
-            start_response(response, [("Content-Type", "text/text;charset=utf-8"),
-                                      ("Content-Length", str(len(content)))])
-            return [content]
+            return await substitution_plan.application_post(environ, start_response)
 
         if environ["REQUEST_METHOD"] == "HEAD":
             if environ["PATH_INFO"] == "/":
                 logger.info("HEAD /")
-                storage = urllib.parse.parse_qs(environ["QUERY_STRING"])
-                response, content = await substitution_plan.get_site_students(storage)
+                await substitution_plan.application_students(environ, start_response)
+                return []
             elif environ["PATH_INFO"] == "/teachers":
                 logger.info("HEAD /teachers")
-                storage = urllib.parse.parse_qs(environ["QUERY_STRING"])
-                response, content = await substitution_plan.get_site_teachers(storage)
+                await substitution_plan.application_teachers(environ, start_response)
+                return []
             elif environ["PATH_INFO"] == "/privacy":
                 logger.info("HEAD /privacy")
                 response = "200 OK"
@@ -270,8 +293,6 @@ async def async_application(environ, start_response):
                 content = await substitution_plan.templates.render_error_404()
 
             content = content.encode("utf-8")
-            t2 = time.perf_counter_ns()
-            logger.debug(f"Time for handling request: {t2 - t1}ns")
             start_response(response, [("Content-Type", "text/html;charset=utf-8"),
                                       ("Content-Length", str(len(content)))])
             return []
@@ -282,6 +303,8 @@ async def async_application(environ, start_response):
                                                   ("Content-Length", str(len(content)))])
         return [content]
     finally:
+        t2 = time.perf_counter_ns()
+        logger.debug(f"Time for handling request: {t2 - t1}ns")
         substitution_plan.stats.save()
 
 
