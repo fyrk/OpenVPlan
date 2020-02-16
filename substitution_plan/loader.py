e0522703c780136e703d686099bd7aab9db7afd7
Florian Rädiker <38762910+FlorianRaediker@users.noreply.github.com>
2020-02-16 07:52:47 +0100

    Fix a bug which stopped some sites from being parsed

--- substitution_plan/loader.py
+++ substitution_plan/loader.py
@@ -33,7 +33,6 @@ class BaseSubstitutionLoader:
         self.substitutions_parser = substitutions_parser_class
         self.url = url
         self.stats = stats
-        self._last_site_num = None
 
     async def load_data(self, session: aiohttp.ClientSession,
                         old_days: Dict[int, Dict[Any, Set[BaseSubstitution]]], first_site=None):
@@ -63,23 +62,28 @@ class BaseSubstitutionLoader:
 
         async def load_from_stream(num, stream: Union[aiohttp.StreamReader, AsyncBytesIOWrapper], request=None):
             logger.debug(f"{self.plan_type}: Loading {num}")
-            nonlocal next_waiting_result
+            nonlocal next_waiting_result, last_site_num
             next_site = await parse_next_site(stream)
             if b"001" == next_site:
                 logger.debug(f"{self.plan_type}: {num} is last site")
-                self._last_site_num = num
+                last_site_num = num
                 for l in loads[num-current_site+1:]:
                     l.cancel()
             results[num-current_site] = (request, stream)
             if next_waiting_result == num:
-                index = num-current_site
+                index = next_waiting_result-current_site
                 results_to_load = [(index+current_site, results[index])]
+                results[index] = None
                 next_waiting_result += 1
-                while True:
+                index += 1
+                logger.debug(f"{self.plan_type}: {num} ({index}, {next_waiting_result}, {last_site_num})")
+                while last_site_num is None or next_waiting_result <= last_site_num:
                     try:
-                        if results[next_waiting_result-current_site] is not None:
-                            index = next_waiting_result-1
+                        logger.debug(f"{self.plan_type}: {num} ({index}, {next_waiting_result}, {last_site_num})")
+                        if results[index] is not None:
                             results_to_load.append((index+current_site, results[index]))
+                            results[index] = None
+                            index += 1
                             next_waiting_result += 1
                         else:
                             break
@@ -87,8 +91,7 @@ class BaseSubstitutionLoader:
                         break
                 await asyncio.gather(*(asyncio.ensure_future(parse_site(num, request, stream, data, current_timestamp))
                                      for num, (request, stream) in results_to_load))
-
-        self._last_site_num = None
+        last_site_num = None
         current_timestamp = create_date_timestamp(datetime.datetime.now())
         data = {}
 
@@ -104,9 +107,9 @@ class BaseSubstitutionLoader:
             for r in results[next_waiting_result:]:
                 if r is not None and r[0] is not None:
                     r[0].close()
-            if self._last_site_num is not None:
+            if last_site_num is not None:
                 if self.stats is not None:
-                    self.stats.add_last_site(self._last_site_num)
+                    self.stats.add_last_site(last_site_num)
                 return self._data_postprocessing(old_days, data)
             current_site = next_site
         else:
@@ -123,9 +126,10 @@ class BaseSubstitutionLoader:
             for r in results[next_waiting_result:]:
                 if r is not None:
                     r[0].close()
-            if self._last_site_num is not None:
+            if last_site_num is not None:
                 if self.stats is not None:
-                    self.stats.add_last_site(self._last_site_num)
+                    self.stats.add_last_site(last_site_num)
+                logger.debug("results: " + str(results))
                 return self._data_postprocessing(old_days, data)
             current_site = next_site
 
