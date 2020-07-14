21af07dc59d078ea711f35c158431e2f0d008780
Florian Rädiker <38762910+FlorianRaediker@users.noreply.github.com>
2020-07-14 16:17:48 +0200

    Fix open website on "notificationclick" event

--- gawvertretung/website/static/sw.js
+++ gawvertretung/website/static/sw.js
@@ -15,7 +15,7 @@ self.addEventListener("push", event => {
         icon: "favicon.ico",
         vibrate: [100, 50, 100],
         data: {
-            url: "/" + data.plan_type + "/",
+            url: new URL("/" + data["plan_type"] + "/", self.location.origin).href,
             dateOfArrival: event.data.status
         }
         /*actions: [
@@ -49,19 +49,17 @@ self.addEventListener("push", event => {
 
 self.addEventListener("notificationclick", event => {
     event.notification.close();
-    event.waitUntil(
-        self.clients.matchAll({type: "window"})
-            .then(clientsArray => {
-                const hasWindow = clientsArray.some(windowClient => {
-                    if (windowClient.url === event.notification.data.url) {
-                        windowClient.focus()
-                        return true;
-                    }
-                    return false;
-                });
-                if (!hasWindow) {
-                    self.clients.openWindow(event.notification.data.url).then(windowClient => windowClient ? windowClient.focus() : null);
-                }
-            })
-    );
-})
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(self.clients.matchAll({
+        type: "window"
+    }).then(function (clientList) {
+        for (let client of clientList) {
+            if (client.url.split("?")[0] === event.notification.data.url && "focus" in client)
+                return client.focus();
+        }
+        if (self.clients.openWindow)
+            return self.clients.openWindow(event.notification.data.url).then(windowClient => windowClient.focus());
+    }));
+});
