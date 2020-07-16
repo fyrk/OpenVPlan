ae6fb0c6032b75956ecf59a08c377b4944d75b72
Florian Rädiker <38762910+FlorianRaediker@users.noreply.github.com>
2020-07-16 12:14:12 +0200

    Always display affected groups in body of notification, never in title

--- gawvertretung/website/static/sw.js
+++ gawvertretung/website/static/sw.js
@@ -24,7 +24,22 @@ self.addEventListener("push", event => {
         timestamp = null;
     }
 
+    let title;
+    let body;
+    if (Object.keys(data["affected_groups_by_day"]).length === 1) {
+        // there is only one day with new substitutions
+        title = Object.keys(data["affected_groups_by_day"])[0] + ": Neue Vertretungen";
+        body = Object.values(data["affected_groups_by_day"])[0].join(", ");
+    } else {
+        title = "Neue Vertretungen";
+        body = "";
+        for (let [dayName, groups] of Object.entries(data["affected_groups_by_day"])) {
+            body += dayName + ": " + groups.join(", ") + "\n";
+        }
+    }
+
     const options = {
+        body: body,
         icon: "favicon.ico",
         badge: "favicon-96-monochrome.png",
         lang: "de",
@@ -34,19 +49,7 @@ self.addEventListener("push", event => {
             url: new URL("/" + data["plan_type"] + "/", self.location.origin).href
         }
     };
-    let title;
-    if (Object.keys(data["affected_groups_by_day"]).length === 1) {
-        // there is only one day with new substitutions
-        title = Object.keys(data["affected_groups_by_day"])[0] + ": Neue Vertretungen für " +
-            Object.values(data["affected_groups_by_day"])[0].join(", ");
-    } else {
-        title = "Neue Vertretungen";
-        let body = "";
-        for (let [dayName, groups] of Object.entries(data["affected_groups_by_day"])) {
-            body += dayName + ": " + groups.join(", ") + "\n";
-        }
-        options["body"] = body;
-    }
+
     event.waitUntil(
         self.registration.showNotification(title, options)
     );
